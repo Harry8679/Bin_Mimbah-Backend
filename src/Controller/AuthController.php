8d25a03b2e8541df79fb305338bd3d60554949a4
Harry EMARH <emarh.harry.code@gmail.com>
2025-04-27 04:19:21 +0200

    Auth Controller Registration

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -2,15 +2,52 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use ApiPlatform\Validator\ValidatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 final class AuthController extends AbstractController
 {
-    #[Route('/auth', name: 'app_auth')]
-    public function index(): Response
+    #[Route('/api/register', name: 'app_register', methods: ['POST'])]
+    public function register(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher, ValidatorInterface $validator): JsonResponse
     {
+        $data = json_decode($request->getContent(), true);
+
+        $firstName = $data['firstName'] ?? null;
+        $lastName = $data['lastName'] ?? null;
+        $phoneNumber = $data['phone_number'] ?? null;
+        $password = $data['password'] ?? null;
+
+        if (!$firstName || !$lastName || $phoneNumber || $password) {
+            return new JsonResponse(['error' => 'Tous les champs sont obligatoires'], 400);
+        }
+
+        $user = new User();
+        $user->setFirstName($firstName);
+        $user->setLastName($lastName);
+        $user->setPhoneNumber($phoneNumber);
+
+        $hashedPassword = $passwordHasher->hashPassword($user, $password);
+        $user->setPassword($hashedPassword);
+
+        $user->setRoles(['ROLES_USER']);
+        $user->setIsVerified(false);
+
+        $errors = $validator->validate($user);
+        if (count($errors) > 0) {
+            return new JsonResponse(['error' => (string) $errors], 400);
+        }
+
+        $em->persist($user);
+        $em->flush();
+
+        return new JsonResponse(['message' => 'Utilisateur enregistré avec succès.'], 201);
+
         return $this->render('auth/index.html.twig', [
             'controller_name' => 'AuthController',
         ]);
